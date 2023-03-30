68cec77ac2ed844cc3b8e960614aebc659e4fd24
LukaszLebioda <lukasz.leb@gmail.com>
2023-03-30 22:42:58 +0200

    folder structure - src, build

--- build/js/main.js
+++ build/js/main.js
@@ -1,16 +1,20 @@
 "use strict";
-let myName = "John";
+let username = "Jimmy";
+console.log(username);
+username = "Lucas";
+let user;
 let meaningOfLife;
 let isLoading;
+isLoading = 200;
 let album1;
 let album2;
-myName = "Lucas";
-meaningOfLife = 42;
-isLoading = true;
-album1 = 40;
-album1 = "40";
-album2 = true;
-const sum = (a, b) => {
-    return a + b;
-};
-console.log(sum(2, "klopsik"));
+// myName = "Lucas";
+// meaningOfLife = 42;
+// isLoading = true;
+// album1 = 40;
+// album1 = "40";
+// album2 = true;
+// const sum = (a: number, b: number) => {
+//     return a + b
+// }
+// console.log(sum(2, "klopsik"));
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,26 @@
-let myName: string = "John";
+let username: string = "Jimmy";
+console.log(username);
+
+username = "Lucas";
+let user: number;
+
 let meaningOfLife: number;
-let isLoading: boolean;
+let isLoading: boolean | number;
+
+isLoading = 200;
+
 let album1: any;
 let album2: number | string | boolean; 
 
-myName = "Lucas";
-meaningOfLife = 42;
-isLoading = true;
+// myName = "Lucas";
+// meaningOfLife = 42;
+// isLoading = true;
 
-album1 = 40;
-album1 = "40";
-album2 = true;
+// album1 = 40;
+// album1 = "40";
+// album2 = true;
 
-const sum = (a: number, b: number) => {
-    return a + b
-}
-console.log(sum(2, "klopsik"));
+// const sum = (a: number, b: number) => {
+//     return a + b
+// }
+// console.log(sum(2, "klopsik"));
